e29c270943ca021eed4202fa63bcbfba12c463ac
aslakbfj <36239161+aslakbfj@users.noreply.github.com>
2024-04-08 18:22:38 +0200

    train

--- data/salmon/train_salmon.sh
+++ data/salmon/train_salmon.sh
@@ -6,29 +6,18 @@
 #SBATCH --mem=30G 			                # Default memory per CPU is 3GB
 #SBATCH --partition=gpu                     # Use GPU partition
 #SBATCH --gres=gpu:1                        # Use one GPU
-#SBATCH --output=./slurm_out/explainn_sal%j_%a.log # Stdout and stderr file
+#SBATCH --output=./slurm_explainn/explainn_salmon%j_%a.log # Stdout and stderr file
 
 
 source activate explainn
 
-
-#### AI-TAC dataset needed
-AI_TAC=$SCRATCH/ExplaiNN/tutorial/AI-TAC
-mm10=$SCRATCH/ExplaiNN/genomes/mm10
-
-
-
-awk 'BEGIN {srand()} {f = FILENAME (rand() <= 0.1 ? ".validation" : ".train");
-print > f}' AtlanticSalmon_bins.tsv
-
-
-
 echo "Train (same parameters as in the preprint; it can take a few hours) and test"
 PY_SCRIPT=../scripts/train.py
-OUT_DIR=$SCRATCH/ExplaiNN/salmon/ExplaiNN/salmon
+OUT_DIR=$SCRATCH/AS-TAC/ExplaiNN/
+
 ${PY_SCRIPT} -o ${OUT_DIR} --input-length 1000 --criterion bcewithlogits \
 --patience 5 \
 --num-epochs 10 \
---num-units 300 AtlanticSalmon_bins.tsv.train \
-AtlanticSalmon_bins.tsv.validation
+--num-units 300 AtlanticSalmon_bins.train.tsv \
+AtlanticSalmon_bins.validation.tsv
 
